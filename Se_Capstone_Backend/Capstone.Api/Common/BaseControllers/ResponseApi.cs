91e0d46107d3c7157dcdcb917c6cfbb76906068a
datpt163 <datpt163@gmail.com>
2024-07-25 21:16:53 +0700

    CreateSolution

---
+++ Se_Capstone_Backend/Capstone.Api/Common/BaseControllers/ResponseApi.cs
@@ -0,0 +1,9 @@
+﻿namespace Capstone.Api.Common.BaseControllers
+{
+    public class ResponseApi
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public object? Data { get; set; }
+    }
+}
