165497e63c1b185279237e1b56f3552f4ec0bde6
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2024-08-04 09:39:04 +0700

    a

---
+++ Se_Capstone_Backend/Capstone.Api/Common/ResponseApi/BaseResponse.cs
@@ -0,0 +1,69 @@
+﻿namespace Capstone.Api.Common.BaseControllers
+{
+    public class BaseResponse
+    {
+        public object Data { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+
+        public BaseResponse(object data, string message, int statusCode)
+        {
+            Data = data;
+            Message = message;
+            StatusCode = statusCode;
+        }
+
+        // Constructor for success response without data
+        public BaseResponse(string message, int statusCode = 200)
+        {
+            Message = message;
+            StatusCode = statusCode;
+        }
+
+        // Constructor for error response
+        public BaseResponse(int statusCode, string message)
+        {
+            Message = message;
+            StatusCode = statusCode;
+        }
+    }
+
+    public class ResponseSuccess : BaseResponse
+    {
+        public ResponseSuccess(object data, string message = "Operation successful.") : base(data, message, 200)
+        {
+        }
+    }
+
+    // Bad request response
+    public class BadRequestResponse : BaseResponse
+    {
+        public BadRequestResponse(string message = "Bad request.") : base(null, message, 400)
+        {
+        }
+    }
+
+    // Not found response
+    public class NotFoundResponse : BaseResponse
+    {
+        public NotFoundResponse(string message = "Resource not found.") : base(null, message, 404)
+        {
+        }
+    }
+
+    // Internal server error response
+    public class InternalServerErrorResponse : BaseResponse
+    {
+        public InternalServerErrorResponse(string message = "An unexpected error occurred.") : base(null, message, 500)
+        {
+        }
+    }
+
+    // Unauthorized response
+    public class UnauthorizedResponse : BaseResponse
+    {
+        public UnauthorizedResponse(string message = "Unauthorized access.") : base(null, message, 401)
+        {
+        }
+    }
+}
--- Se_Capstone_Backend/Capstone.Api/Module/Account/Controllers/AccountController.cs
+++ Se_Capstone_Backend/Capstone.Api/Module/Account/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Capstone.Api.Module.Account.Controllers
@@ -7,5 +8,16 @@ namespace Capstone.Api.Module.Account.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        [HttpGet("Auth")]
+        public async Task<IActionResult> AuthAsync()
+        {
+            //(string accessToken, string errorMessage) = await AccountService.AuthAsync(accountAuthRequest);
+            //if (!string.IsNullOrEmpty(errorMessage))
+            //{
+            //    return ResponseBadRequest(errorMessage);
+            //}
+            //return ResponseOk(new { accessToken });
+            return Ok(new { });
+        }
     }
 }
