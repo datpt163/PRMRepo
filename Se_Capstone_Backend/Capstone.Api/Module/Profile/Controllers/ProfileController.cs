d744859af312722f278ffa298dd049a496b69ba8
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2024-08-12 19:16:30 +0700

    feature: updateProfile

---
+++ Se_Capstone_Backend/Capstone.Api/Module/Profile/Controllers/ProfileController.cs
@@ -0,0 +1,35 @@
+﻿using Capstone.Api.Common.ResponseApi.Controllers;
+using Capstone.Api.Common.ResponseApi.Model;
+using Capstone.Application.Module.Account.Query;
+using Capstone.Application.Module.Account.Response;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Capstone.Api.Module.Profile.Controllers
+{
+    [Route("api/profile")]
+    [ApiController]
+    public class ProfileController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public ProfileController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [SwaggerResponse(200, "Successful", typeof(ResponseSuccess<LoginResponse>))]
+        [SwaggerResponse(400, "Fail", typeof(ResponseFail))]
+        [HttpPut("auth")]
+        public async Task<IActionResult> Auth([FromBody] LoginQuery loginQuery)
+        {
+            var result = await _mediator.Send(loginQuery);
+
+            if (string.IsNullOrEmpty(result.ErrorMessage))
+                return ResponseOk(dataResponse: result.Data);
+            return ResponseBadRequest(messageResponse: result.ErrorMessage);
+        }
+    }
+}
