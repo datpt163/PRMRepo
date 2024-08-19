a7f5f069130a46be889166d89994f4ea7b7e9610
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2024-08-19 19:49:52 +0700

    create db

---
+++ Se_Capstone_Backend/Capstone.Domain/Entities/Applicant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Domain.Entities
+{
+    public class Applicant
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string CvLink { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsOnBoard { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public Guid StaffId { get; set; }
+        public Staff Staff { get; set; } = new Staff();
+
+        public ICollection<Job> Jobs { get; set; } = new List<Job>();
+    }
+}
---
+++ Se_Capstone_Backend/Capstone.Domain/Entities/Job.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Domain.Entities
+{
+    public class Job
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public ICollection<Applicant> Applicants { get; set; } = new List<Applicant>();
+    }
+}
