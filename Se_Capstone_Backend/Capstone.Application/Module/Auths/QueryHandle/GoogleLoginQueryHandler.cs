3e930a2e83b29d16758730b021e34fca24c818d9
iphongnv0910 <i_phong.nv@3si.vn>
2024-09-04 17:51:17 +0700

    [Add] login by google

---
+++ Se_Capstone_Backend/Capstone.Application/Module/Auths/QueryHandle/GoogleLoginQueryHandler.cs
@@ -0,0 +1,76 @@
+﻿using Capstone.Application.Common.Jwt;
+using Capstone.Application.Common.ResponseMediator;
+using Capstone.Application.Module.Auth.Response;
+using Capstone.Application.Module.Auths.Query;
+using Capstone.Domain.Entities;
+using Capstone.Infrastructure.Repository;
+using Google.Apis.Auth;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Application.Module.Auths.QueryHandle
+{
+    public class GoogleLoginQueryHandler : IRequestHandler<GoogleLoginQuery, ResponseMediator>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IJwtService _jwtService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GoogleLoginQueryHandler(UserManager<User> userManager, IJwtService jwtService, IUnitOfWork unitOfWork)
+        {
+            _userManager = userManager;
+            _jwtService = jwtService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ResponseMediator> Handle(GoogleLoginQuery request, CancellationToken cancellationToken)
+        {
+            var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken);
+            if (payload == null)
+            {
+                return new ResponseMediator("Invalid Google token", null, 400);
+            }
+
+            var user = await _userManager.FindByEmailAsync(payload.Email);
+            if (user == null)
+            {
+                user = new User
+                {
+                    UserName = payload.Email,
+                    Email = payload.Email,
+                    FullName = payload.Name
+                };
+
+                var result = await _userManager.CreateAsync(user);
+                if (!result.Succeeded)
+                {
+                    return new ResponseMediator("User creation failed", null, 400);
+                }
+
+                await _userManager.AddToRoleAsync(user, "User");
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+            var accessToken = await _jwtService.GenerateJwtTokenAsync(user, DateTime.Now.AddDays(10));
+            var refreshToken = await _jwtService.GenerateJwtTokenAsync(user, DateTime.Now.AddDays(30));
+
+            var role = roles.FirstOrDefault();
+            var roleEntity = _unitOfWork.Roles.Find(x => x.Name == role).FirstOrDefault();
+
+            return new ResponseMediator("", new LoginResponse
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken,
+                UserId = user.Id,
+                RoleName = role,
+                RoldId = roleEntity?.Id
+            });
+        }
+    }
+
+}
