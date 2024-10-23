3faa5049eb5bf7f4e97a1ea27030e02699f7b67f
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2024-10-23 21:25:32 +0700

    [fix] crud default label

---
+++ Se_Capstone_Backend/Capstone.Application/Module/Labels/Command/CreateLabelDefaultCommand.cs
@@ -0,0 +1,16 @@
+﻿using Capstone.Application.Common.ResponseMediator;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Application.Module.Labels.Command
+{
+    public class CreateLabelDefaultCommand : IRequest<ResponseMediator>
+    {
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+    }
+}
---
+++ Se_Capstone_Backend/Capstone.Application/Module/Labels/Command/UpdateDefaultLabelCommand.cs
@@ -0,0 +1,17 @@
+﻿using Capstone.Application.Common.ResponseMediator;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Application.Module.Labels.Command
+{
+    public class UpdateDefaultLabelCommand : IRequest<ResponseMediator>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+    }
+}
