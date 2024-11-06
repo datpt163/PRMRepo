89ac77e2aed41c7b1ca476e4addb5b020ec8b2ee
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2024-11-06 22:40:46 +0700

    [fix] masstransit

--- Se_Capstone_Backend/Capstone.Application/Module/Issues/ConsumerRabbitMq/AddIssueConsumer.cs
+++ Se_Capstone_Backend/Capstone.Application/Module/Issues/ConsumerRabbitMq/AddIssueConsumer.cs
@@ -5,6 +5,7 @@ using Capstone.Domain.Entities;
 using Capstone.Domain.Enums;
 using Capstone.Infrastructure.Repository;
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,12 +25,20 @@ namespace Capstone.Application.Module.Issues.ConsumerRabbitMq
 
         public async Task Consume(ConsumeContext<AddIssueMessage> context)
         {
-            if(context.is)
-            await _unitOfWork.Issues.Add(issue);
-            await _unitOfWork.SaveChangesAsync();
-            var issueResponse = _unitOfWork.Issues.Find(x => x.Id == issue.Id).Include(c => c.Phase).Include(c => c.Label).Include(c => c.Status).Include(c => c.LastUpdateBy).Include(c => c.ParentIssue).Include(c => c.Reporter).Include(c => c.Assignee).Include(c => c.SubIssues).Include(c => c.Comments).FirstOrDefault();
-            var response = _mapper.Map<IssueDTO?>(issueResponse);
-            return new ResponseMediator("", response);
+            var issue = context.Message.Issue;
+            if (issue != null)
+            {
+                var status = _unitOfWork.Statuses.Find(x => x.Id == context.Message.StatusId).Include(c => c.Project).ThenInclude(c => c.Phases).Include(c => c.Issues).FirstOrDefault();
+                if (status == null)
+                    return;
+                var position = status.Issues.Count() + 1;
+                issue.Position = position;
+                _unitOfWork.Issues.Add(issue);
+                await _unitOfWork.SaveChangesAsync();
+                //var issueResponse = _unitOfWork.Issues.Find(x => x.Id == issue.Id).Include(c => c.Phase).Include(c => c.Label).Include(c => c.Status).Include(c => c.LastUpdateBy).Include(c => c.ParentIssue).Include(c => c.Reporter).Include(c => c.Assignee).Include(c => c.SubIssues).Include(c => c.Comments).FirstOrDefault();
+                //var response = _mapper.Map<IssueDTO?>(issueResponse);
+                //return new ResponseMediator("", response);
+            }
         }
     }
 }
