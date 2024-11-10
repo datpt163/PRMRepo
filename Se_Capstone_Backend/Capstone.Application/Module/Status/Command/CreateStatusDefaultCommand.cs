ff3536709fac52dd56ba8aee23a5d2bdc5cff189
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2024-11-10 16:49:04 +0700

    [refactor] add field is done in status

--- Se_Capstone_Backend/Capstone.Application/Module/Status/Command/CreateStatusDefaultCommand.cs
+++ Se_Capstone_Backend/Capstone.Application/Module/Status/Command/CreateStatusDefaultCommand.cs
@@ -8,5 +8,6 @@ namespace Capstone.Application.Module.Status.Command
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; } = string.Empty;
         public string Color { get; set; } = string.Empty;
+        public bool IsDone { get; set; }
     }
 }
